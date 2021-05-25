b304de7a1f734ac3926f5447cfeb508fc17aa4cd
rishi860 <rishi860kh@gmail.com>
2021-05-25 12:21:15 +0530

    vue header

---
+++ client/src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  }
+//   {
+//     path: '/songs/create',
+//     name: 'create-songs',
+//     component: CreateSong
+//   },
+//   {
+//     path: '/songs/:songId/edit',
+//     name: 'edit',
+//     component: EditSong
+//   }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
