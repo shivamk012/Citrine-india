33018beaf5d32cc043e69cb23722585309703c38
rishi860 <rishi860kh@gmail.com>
2021-06-05 17:54:14 +0530

    toggle button

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,62 +1,51 @@
 <template>
-  <div>
+  <nav>
     <v-toolbar
       color="#f2eecb"
       dark
       fixed
       flat
+      app
     >
       <v-toolbar-title class="white--text logo" @click="navigateTo({name:'home'})">
         CITRINE-INDIA
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items
+        class="d-none d-sm-flex"
+      >
         <v-btn
-          v-if="!$store.state.isUserLoggedin"
-          depressed flat
+          depressed
+          flat
           color="#f2eecb"
-          class="white--text"
-          @click="navigateTo({
-            name: 'signUp'
-          })"
+          v-for="item in menuItems"
+          :key="item.title"
         >
-          Sign Up
-        </v-btn>
-        <v-btn
-          depressed flat
-          color="#f2eecb"
-          class="black--text"
-        >
-          Browse
-        </v-btn>
-        <v-btn
-          v-if="$store.state.isUserLoggedin"
-          depressed light
-          color="#f2eecb"
-          @click="logout()"
-        >
-          Log Out 
-        </v-btn>
-        <v-btn
-          v-if="!$store.state.isUserLoggedin"
-          depressed light
-          color="#f2eecb"
-          @click="navigateTo({name:'login'})"
-        >
-          Login
-        </v-btn>
-        <v-btn
-          v-if="$store.state.isAdmin"
-          depressed light
-          color="#f2eecb"
-          @click="navigateTo({name:'adminHome'})"
-        >
-          Admin
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.title }}
         </v-btn>
       </v-toolbar-items>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        @click="sideNav = !sideNav"
+        class="d-flex d-sm-none"
+      ></v-app-bar-nav-icon>
     </v-toolbar>
-  </div>
+    <v-navigation-drawer v-model="sideNav" app class="d-lg-none d-xl-flex" right>
+      <v-list>
+        <v-list-item-group>
+          <v-list-item
+            v-for="item in menuItems"
+            :key="item.title"
+          >
+            <v-list-item-title>
+              <v-icon>{{ item.icon }}</v-icon>
+              {{ item.title }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
@@ -70,7 +59,16 @@
         this.$store.dispatch('setToken', null)
         this.$store.dispatch('setUser', null)
       }
-    }
+    },
+    data: () => ({
+      sideNav: false,
+      menuItems: [
+        {icon:'mdi-folder-open',title:'Collections'},
+        {icon:'mdi-account',title:'Profile'},
+        {icon:'mdi-account-group-online',title:'Sign Up'},
+        {icon:'mdi-account',title:'Sign In'},
+      ]
+    })
   }
 </script>
 
