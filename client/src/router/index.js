ebdb812446b377bf440b52dbd7882962e239c58d
shivamk012 <shivamkoolwal14@gmail.com>
2023-01-19 17:26:29 +0530

    changes for deployment

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,3 @@
-
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/HomePage/Home.vue'
--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   mongo: {
-    uri: process.env.MONGODB_URI || `mongodb://localhost:27017/citrine`,
+    uri: `mongodb+srv://shivam:shivam@cluster0.4epaz.mongodb.net/Ecommerce?retryWrites=true&w=majority` || `mongodb://localhost:27017/citrine`,
   },
   port: process.env.PORT || '8081'
 };
